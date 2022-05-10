1db09978d24f6ac4f19ba8d7ba67f18d1d057937
hydos <haydenv06@gmail.com>
2022-05-10 14:46:13 +1000

    Dark and Light mode. Yes

---
+++ darkMode.js
@@ -0,0 +1,53 @@
+function darkMode() {
+  document.querySelectorAll(".bg-light").forEach((element) => {
+    element.className = element.className.replace(/-light/g, "-dark");
+  });
+
+  document.querySelectorAll(".text-dark").forEach((element) => {
+    element.className = element.className.replace(/-dark/g, "-light");
+  });
+
+  document.body.classList.replace("bg-light", "bg-dark");
+
+  if (document.body.classList.contains("text-dark")) {
+    document.body.classList.replace("text-dark", "text-light");
+  } else {
+    document.body.classList.add("text-light");
+  }
+
+  document.getElementById("moon").hidden = true;
+  document.getElementById("sun").hidden = false;
+}
+
+function lightMode() {
+  document.querySelectorAll(".bg-dark").forEach((element) => {
+    element.className = element.className.replace(/-dark/g, "-light");
+  });
+
+  document.querySelectorAll(".text-light").forEach((element) => {
+    element.className = element.className.replace(/-light/g, "-dark");
+  });
+
+  document.body.classList.replace("bg-dark", "bg-light");
+
+  if (document.body.classList.contains("text-light")) {
+    document.body.classList.replace("text-light", "text-dark");
+  } else {
+    document.body.classList.add("text-dark");
+  }
+
+  document.getElementById("moon").hidden = false;
+  document.getElementById("sun").hidden = true;
+}
+
+function getSystemDefaultTheme() {
+  const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+  if (darkThemeMq.matches) {
+    return "dark";
+  }
+  return "light";
+}
+
+if(getSystemDefaultTheme() === "dark") {
+  darkMode();
+}
